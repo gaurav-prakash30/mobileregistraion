0a9163a42d7a5b5c29be545cf3b0ed96d52a5e60
Dhirendra Singh <dhirendra.singh@AD.SAASFOCUS.COM>
2019-02-25 18:54:38 +0530

    api log added

--- app.js
+++ app.js
@@ -28,18 +28,33 @@ client.connect();
      var device_ID = req.body.device_ID;
      var mpin = req.body.mpin;
 
-    client.query('UPDATE salesforce.contact SET IVL_Device_Id__c = ($1), IVL_MPIN__c=($2)  WHERE sfid = ($3) RETURNING sfid',
+    client.query('UPDATE salesforce.contact SET IVL_Device_Id__c = ($1), IVL_MPIN__c=($2)  WHERE sfid = ($3)',
      [req.body.device_ID, req.body.mpin, req.body.con_id],
      function(err, result) {
-         if (err){
-             throw err;
+         if (err)
+         {
+            var apiresponse = {sucess:true,error_message: err}
+            client.query('INSERT INTO salesforce.IVL_Error_Log__c (IVL_API_Name__c,IVL_Is_Error_Exception__c,IVL_Contact__c,IVL_Request__c,IVL_Response__c,IVL_Type__c) VALUES(\'mobileregistration\',false,($1),($2),($3),\'inbound\')',
+            [req.body.con_id, req.body, JSON.stringify(apiresponse)],
+            function(err, result) 
+            {
+                if (err)
+                    throw err;   
+            }
+            );
          }
-         else{
-             //res.send('Records updated successfully!');
-             res.send(JSON.stringify(res.rows));
-         }
-     }
-    );
+         else
+         {
+            var apiresponse = {sucess:true,error_message: null}
+            client.query('INSERT INTO salesforce.IVL_Error_Log__c (IVL_API_Name__c,IVL_Is_Error_Exception__c,IVL_Contact__c,IVL_Request__c,IVL_Response__c,IVL_Type__c) VALUES(\'mobileregistration\',false,($1),($2),($3),\'inbound\')',
+            [req.body.con_id, req.body, JSON.stringify(apiresponse)],
+            function(err, result) 
+            {
+                if (err)
+                    throw err;   
+            }
+            );
+        }   
  });
 
  app.set('port', process.env.PORT || 3001);
